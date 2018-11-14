3622b6817e061d002ca74157070561e18dc6d77c
liuzhouliang <525688067@qq.com>
2018-11-14 18:52:45 +0800

    kotlin

--- TestSingle/src/main/java/com/example/myapplication/Single4.kt
+++ TestSingle/src/main/java/com/example/myapplication/Single4.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
 /**
- * @ describe
+ * @ describe 显式声明构造方法为private
+companion object用来在class内部声明一个对象
+LazySingleton的实例instance 通过lazy来实现懒汉式加载
+lazy默认情况下是线程安全的，这就可以避免多个线程同时访问生成多个实例的问题
  *
  * @author lzl
  *
---
+++ TestSingle/src/main/java/com/example/myapplication/Test.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+/**
+ * @author lzl
+ * @ describe
+ * @ time 2018/11/14 18:41
+ */
+public class Test {
+    public void test(){
+        Single1.INSTANCE.getName();
+    }
+}

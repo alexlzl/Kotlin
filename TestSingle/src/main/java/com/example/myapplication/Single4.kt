f5593384d10e6ef8708100161e575eeb171a4db5
liuzhouliang <525688067@qq.com>
2018-11-14 18:55:46 +0800

    kotlin

--- TestSingle/src/main/java/com/example/myapplication/Single4.kt
+++ TestSingle/src/main/java/com/example/myapplication/Single4.kt
@@ -5,6 +5,11 @@ package com.example.myapplication
 companion object用来在class内部声明一个对象
 LazySingleton的实例instance 通过lazy来实现懒汉式加载
 lazy默认情况下是线程安全的，这就可以避免多个线程同时访问生成多个实例的问题
+
+关于如何选择饿汉式还是懒汉式，通常应该从两方面考虑
+实例初始化的性能和资源占用
+编写的效率和简洁
+对于实例初始化花费时间较少，并且内存占用较低的话，应该使用object形式的饿汉式加载。否则使用懒汉式。
  *
  * @author lzl
  *

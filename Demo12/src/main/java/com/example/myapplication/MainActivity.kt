e3721c2181bdab169c3eaec2ac06ed9559fe7999
liuzhouliang <525688067@qq.com>
2018-11-27 14:31:14 +0800

    kotlin

--- Demo12/src/main/java/com/example/myapplication/MainActivity.kt
+++ Demo12/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var name=Test4().name
+        //对应生成的java代码调用的是geName方法，name属性在生成的java代码中是private类型
+        var name = Test4().name
     }
 }
